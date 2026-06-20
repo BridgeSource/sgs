16c19c30ff8e2f89186574a1bfa8ff2707a1dc8f
drporterfield <92737951+drporterfield@users.noreply.github.com>
2026-06-20 10:11:49 -0500

    Update notion-hugo.config.ts page

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -4,7 +4,7 @@ const userConfig: UserConfig = {
     base_url: "https://notion-hugo.pages.dev",
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://app.notion.com/p/Notion-Hugo-51eeea78fd4a83459f7c010953aa8450?source=copy_link',
         pages: [
             // {
             //     page_id: '<page_id>',
